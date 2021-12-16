489829e8942ab1f5e15d3c4fb3d6e97eabc7483f
Steven Killoran <43772313+Ambrotus@users.noreply.github.com>
2021-12-16 14:39:21 -0500

    Add files via upload

---
+++ Source/boxSelectChatbox.py
@@ -0,0 +1,92 @@
+import numpy as np
+import cv2 as cv2
+from PIL import ImageGrab
+import mss
+
+# Soloution to the hard coding problem is to just hardcode a ratio in which to resize display and then resize back to full!
+#
+# sct = mss.mss()
+# image = np.asarray(sct.grab(sct.monitors[-1]))
+# (origH, origW) = image.shape[:2]
+# ratioW = 1.5#origW / newW
+# ratioH = 1.5#origH / newH
+# newW = origH/ratioW
+# newH = origW/ratioH
+# image = cv2.resize(image, (int(newH), int(newW)))
+# cv2.imshow("image",image)
+# cv2.waitKey(0)
+def getCoords():
+
+    # Taking screenshot from primary monitor, this can be changed later check mss documentation
+    # with mss.mss() as sct:
+    # # The screen part to capture
+    #     monitor = {"top": 160, "left": 160, "width": 160, "height": 135}
+    #     output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
+
+    #     # Grab the data
+    #     sct_img = sct.grab(monitor)
+
+    # with mss.mss() as sct:
+    sct = mss.mss()
+    # -1 i think gets primary monitor? 0 is all and then increments to the number they identify as. for example my primary is 2
+    image = np.asarray(sct.grab(sct.monitors[1]))
+    # cv2.imshow("filename",img)
+    # cv2.waitKey(0)
+
+    # deskimage = ImageGrab.grab()
+    # image = np.array(deskimage)
+    (origH, origW) = image.shape[:2]
+    # set the new width and height and then determine the ratio in change
+    # for both the width and height
+    # (newW, newH) = (1366,768)
+    ratio = 1.5#origW / newW
+    # ratioH = 1.5#origH / newH
+    newW = origH/ratio
+    newH = origW/ratio
+    # resize the image and grab the new image dimensions
+    image = cv2.resize(image, (int(newH), int(newW)))
+    # (H, W) = image.shape[:2]
+
+    def areaOfInterest(x,y,w,h):
+        return(x, y, x+w, y+h)
+
+    def cropAreaOfInterest(x,y,x2,y2,image):
+        return image[y:y2, x:x2]
+
+    bbox = cv2.selectROI("Drag Box Around Chat",image,False)
+    print(bbox)
+    # bbox returns x,y,w,h
+    # use area of interest function to get starting and ending x,y coords for image
+    x,y,x2,y2 = areaOfInterest(bbox[0],bbox[1],bbox[2],bbox[3])
+
+    # deskimage = ImageGrab.grab()
+    # image2 = np.array(deskimage)
+    # cropImg2 = cropAreaOfInterest(int(x*ratioW),int(y*ratioH),int(x2*ratioW),int(y2*ratioH),image2)
+    # cv2.imshow('cropImg2', cropImg2)
+    # cv2.waitKey(1)
+    cv2.destroyWindow("Drag Box Around Chat")
+    # return (int(x*ratioW),int(y*ratioH),int(x2*ratioW),int(y2*ratioH))
+    # using mss as our new screenshot tool i think i can just return the bbox coords after making them 1.5x bigger
+    return (int(bbox[0]*ratio),int(bbox[1]*ratio),int(bbox[2]*ratio),int(bbox[3]*ratio))
+
+
+def main():
+    ChatCoord = getCoords()
+    x = ChatCoord[0]
+    y = ChatCoord[1]
+    w= ChatCoord[2]
+    h= ChatCoord[3]
+    sct = mss.mss()
+    # -1 i think gets primary monitor? 0 is all and then increments to the number they identify as. for example my primary is 2
+    monitor = {"top": y, "left": x, "width": w, "height": h}
+    image = np.asarray(sct.grab(monitor))
+    cv2.imshow("image",image)
+    cv2.waitKey(0)
+    # global gui_queue
+    # global ocr_queue
+    # ocr_queue = mp.Queue()
+    # gui_queue = mp.Queue()
+    # # gui = GraphicalUserInterface()
+    # startOcr(gui_queue,ocr_queue,(560,595,500,300))
+if __name__ == '__main__':
+    main()
---
+++ Source/gameOverlay.py
@@ -0,0 +1,98 @@
+#   Todo -
+#       main menu, Choose langages : this can control masking color, translation language target and easyocr language reader
+#       implement boxSelectChatbox Location to the main menu when user doesnt want to use default location
+#       front end with node/electron
+
+# https://python-mss.readthedocs.io/examples.html
+# https://www.codegrepper.com/code-examples/python/mss+grab+part+of+screen
+
+# from easyocr import Reader
+# import numpy as np
+# import cv2 as cv2
+# from PIL import ImageGrab
+# https://realpython.com/pysimplegui-python/
+import PySimpleGUI as sg
+# import time
+import torch.multiprocessing as mp
+# from deep_translator import GoogleTranslator
+# import mss
+# gui_queue = None
+# ocr_queue = None
+
+
+class GameOverlayInterface: #rename to gameoverlayinterface
+    global gui_queue
+    global ocr_queue
+    def __init__(self,gQueue,oQueue):
+        self.gui_queue = gQueue
+        self.ocr_queue = oQueue
+        # global gui_queue
+        # global ocr_queue
+        sg.theme('DarkBlack') # give our window a spiffy set of colors
+
+        layout = [
+                [sg.Output(size=(50, 10), font=('Helvetica', '8','bold'), text_color = 'White', key = '_OUTPUT_',background_color= 'grey')],
+                [sg.Button('Exit')]]#sg.Button('Start'),
+
+        self.window = sg.Window('Chat window', layout, font=('Helvetica', ' 13','bold'),background_color='grey', default_button_element_size=(1,1), use_default_focus=False,
+                    transparent_color='grey',alpha_channel=1,titlebar_background_color='black', no_titlebar=True,grab_anywhere=True,keep_on_top=True,enable_close_attempted_event=True, location=sg.user_settings_get_entry('-location-', (None, None)))
+
+        while True:  # Event Loop
+            event, values = self.window.read(timeout=10, timeout_key='timeout')#,timeout_key='timeout'timeout=500
+            # self.window.KeepOnTop = True
+            self.window.bring_to_front()
+            # print(event, values)
+            # print("test take over")
+            # self.window['_OUTPUT_'].Update('')
+            if event in (sg.WINDOW_CLOSE_ATTEMPTED_EVENT, 'Exit'):
+                # gui_queue.put('Stop')
+                sg.user_settings_set_entry('-location-', self.window.current_location())  # The line of code to save the position before exiting
+                break
+            # if event == 'Start':
+            #     # Update the "output" text element to be the value of "input" element
+            #     self.window['_OUTPUT_'].update("started")
+            #     begin(gui_queue, ocr_queue)
+            self.printOutput()
+
+            # print('realtest')
+            # self.window.refresh()
+            # self.window.TKroot.after(1000, self.printOutput)
+        self.window.close()
+
+    def printOutput(self):
+        # self.window['_OUTPUT_'].Update('')
+        # print(ocr_queue.get())
+        outText = []
+        try:
+            for msg in self.ocr_queue.get(0):
+                if msg[0] == r'[':
+                    continue
+        # https://medium.com/analytics-vidhya/how-to-translate-text-with-python-9d203139dcf5
+                # print(msg)
+                outText.append(msg)#+'\n'
+            self.window['_OUTPUT_'].Update(''.join(outText))
+        except:
+            pass
+        # self.window.refresh()
+        # self.window.TKroot.after(1000, self.printOutput)
+
+
+
+
+
+def main():
+    global gui_queue
+    global ocr_queue
+    ocr_queue = mp.Queue()
+    gui_queue = mp.Queue()
+    gui = GameOverlayInterface(gui_queue,ocr_queue)
+    # startOcr(gui_queue,ocr_queue,(560,595,500,300))
+
+if __name__ == '__main__':
+    main()
+
+# # https://github.com/PySimpleGUI/PySimpleGUI/issues/1077
+# # https://github.com/PySimpleGUI/PySimpleGUI/blob/master/DemoPrograms/Demo_Notification_Window_Multiprocessing.py
+# # https://stackoverflow.com/questions/60213167/pysimplegui-how-to-make-transparent-click-through-window
+# # https://github.com/PySimpleGUI/PySimpleGUI/issues/2525
+# # https://docs.python.org/3/library/multiprocessing.html

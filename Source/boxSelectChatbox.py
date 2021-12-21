c6355da0e6233b7c03c1882140dc3c1681e996a9
Steven Killoran <43772313+Ambrotus@users.noreply.github.com>
2021-12-21 14:06:54 -0500

    Made sure to get primary monitor

--- Source/boxSelectChatbox.py
+++ Source/boxSelectChatbox.py
@@ -17,30 +17,14 @@ import mss
 # cv2.waitKey(0)
 def getCoords():
 
-    # Taking screenshot from primary monitor, this can be changed later check mss documentation
-    # with mss.mss() as sct:
-    # # The screen part to capture
-    #     monitor = {"top": 160, "left": 160, "width": 160, "height": 135}
-    #     output = "sct-{top}x{left}_{width}x{height}.png".format(**monitor)
-
-    #     # Grab the data
-    #     sct_img = sct.grab(monitor)
-
-    # with mss.mss() as sct:
     sct = mss.mss()
-    # -1 i think gets primary monitor? 0 is all and then increments to the number they identify as. for example my primary is 2
-    image = np.asarray(sct.grab(sct.monitors[-1]))
-    # cv2.imshow("filename",img)
-    # cv2.waitKey(0)
+    # https://stackoverflow.com/questions/4391697/find-the-index-of-a-dict-within-a-list-by-matching-the-dicts-value
+    primary = next((index for (index, d) in enumerate(sct.monitors) if d["left"] == 0), -1)
+    image = np.asarray(sct.grab(sct.monitors[primary]))
 
-    # deskimage = ImageGrab.grab()
-    # image = np.array(deskimage)
     (origH, origW) = image.shape[:2]
-    # set the new width and height and then determine the ratio in change
-    # for both the width and height
-    # (newW, newH) = (1366,768)
-    ratio = 1.5#origW / newW
-    # ratioH = 1.5#origH / newH
+    ratio = 1.5
+
     newW = origH/ratio
     newH = origW/ratio
     # resize the image and grab the new image dimensions
@@ -59,11 +43,6 @@ def getCoords():
     # use area of interest function to get starting and ending x,y coords for image
     x,y,x2,y2 = areaOfInterest(bbox[0],bbox[1],bbox[2],bbox[3])
 
-    # deskimage = ImageGrab.grab()
-    # image2 = np.array(deskimage)
-    # cropImg2 = cropAreaOfInterest(int(x*ratioW),int(y*ratioH),int(x2*ratioW),int(y2*ratioH),image2)
-    # cv2.imshow('cropImg2', cropImg2)
-    # cv2.waitKey(1)
     cv2.destroyWindow("Drag Box Around Chat and press enter or c to cancel")
     # return (int(x*ratioW),int(y*ratioH),int(x2*ratioW),int(y2*ratioH))
     # using mss as our new screenshot tool i think i can just return the bbox coords after making them 1.5x bigger
@@ -77,16 +56,10 @@ def main():
     w= ChatCoord[2]
     h= ChatCoord[3]
     sct = mss.mss()
-    # -1 i think gets primary monitor? 0 is all and then increments to the number they identify as. for example my primary is 2
+
     monitor = {"top": y, "left": x, "width": w, "height": h}
     image = np.asarray(sct.grab(monitor))
     cv2.imshow("image",image)
     cv2.waitKey(0)
-    # global gui_queue
-    # global ocr_queue
-    # ocr_queue = mp.Queue()
-    # gui_queue = mp.Queue()
-    # # gui = GraphicalUserInterface()
-    # startOcr(gui_queue,ocr_queue,(560,595,500,300))
 if __name__ == '__main__':
     main()
